0820e9e0ff2a6725cdebdb70f582ba17eb9cf165
dpnzdy <dpnzdy@gmail.com>
2026-09-10 23:11:59 +0900

    Add product imagery and fix local routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://zinniashark.com',
   integrations: [sitemap()],
-  trailingSlash: 'never'
+  trailingSlash: 'always'
 });
